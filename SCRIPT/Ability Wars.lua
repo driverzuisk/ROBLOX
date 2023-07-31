76389c653f52898f5d8309f07d4346a7d2363bca
driverzuisk <136741043+driverzuisk@users.noreply.github.com>
2023-07-31 20:57:41 +0200

    Update Ability Wars.lua

--- SCRIPT/Ability Wars.lua
+++ SCRIPT/Ability Wars.lua
@@ -40,7 +40,7 @@ MAIN_TAB:AddToggle({
 		getgenv().saura = bool
 
       while getgenv().saura do
-         task.wait(2)
+         task.wait(1.25)
       for i,v in pairs(game:GetService("Players"):GetChildren()) do
          if v.Character["Right Arm"]:FindFirstChild("SelectionBox") == nil and v ~= game.Players.LocalPlayer then
          local target = v.Character.HumanoidRootPart
@@ -208,7 +208,7 @@ game.Players.LocalPlayer.Chatted:Connect(function(msg)
 				getgenv().aura = bool
 		
 			  while getgenv().aura do
-				 task.wait(2)
+				 task.wait(1.25)
 			  for i,v in pairs(game:GetService("Players"):GetChildren()) do
 				 if v.Character["Right Arm"]:FindFirstChild("SelectionBox") == nil and v ~= game.Players.LocalPlayer then
 				 local target = v.Character.HumanoidRootPart
